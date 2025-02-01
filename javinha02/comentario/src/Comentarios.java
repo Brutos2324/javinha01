ede4a9ef70123bc3840059650177d675a71ab781
Brutos2324 <fvlbrutos212@gmail.com>
2025-01-31 23:32:52 -0300

    comentario em Java

---
+++ javinha02/comentario/src/Comentarios.java
@@ -0,0 +1,12 @@
+/**
+ * @version
+ * @since /01/2024
+ */
+
+//Aula sobre Comentario
+
+public class Comentarios {
+    public static void main(String[] args) {
+        System.out.println("Primeira Linha");
+    }
+}
